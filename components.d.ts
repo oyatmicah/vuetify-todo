7542f9a69509b0c6040457dfa19fa5f12949773b
oyatmicah <oyatmicah@gmail.com>
2024-04-07 14:35:56 +0100

    done1

--- components.d.ts
+++ components.d.ts
@@ -8,7 +8,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AppBar: typeof import('./src/components/AppBar.vue')['default']
-    copy: typeof import('./src/components/SideNavigation copy.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
